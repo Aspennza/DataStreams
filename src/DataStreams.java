d70ba4cf4d241354c2ad5f5ab00896e4b5240c33
Zoe Aspenns <aspennza@mail.uc.edu>
2025-11-10 10:34:19 -0500

    Completed JavaDoc

--- src/DataStreams.java
+++ src/DataStreams.java
@@ -15,52 +15,115 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 // To do:
-// javadoc
 // uml
 
+/**
+ * Allows the creation of objects for reading and filtering files using Streams.
+ * Contains a GUI, methods for reading and filtering files, and a method for
+ * resetting the program.
+ * @author Zoe Aspenns aspennza@mail.uc.edu
+ */
 public class DataStreams
 {
+    //A JPanel containing all other panels in the GUI
     JPanel mainPnl;
+
+    //A JPanel containing a title for the JFrame
     JPanel titlePnl;
+
+    //A JLabel acting as the visible title for the JFrame
     JLabel titleLbl;
+
+    //The Font applied to the titleLbl
     Font titleFont;
+
+    //A JPanel containing the name of the file being read and the string users want to search on
     JPanel fileSearchPnl;
+
+    //A JLabel for the fileNameTF field
     JLabel fileNameLbl;
+
+    //A JTextField displaying the name of the file being read
     JTextField fileNameTF;
+
+    //A JLabel for the searchStringTF
     JLabel searchStringLbl;
+
+    //A JTextField where users can enter their desired search string
     JTextField searchStringTF;
+
+    //A JPanel where the original text of the file and the filtered text of the file will be displayed
     JPanel fileDisplayPnl;
+
+    //A JLabel for the originalFileTA
     JLabel originalFileLbl;
+
+    //A JLabel for the filteredFileTA
     JLabel filteredFileLbl;
+
+    //A JTextArea for displaying the original text of the file being read
     JTextArea originalFileTA;
+
+    //A JTextArea for displaying the filtered text of the file being read
     JTextArea filteredFileTA;
+
+    //A JScrollPane for the originalFileTA
     JScrollPane scroller1;
+
+    //A JScrollPane for the filteredFileTA
     JScrollPane scroller2;
+
+    //A JPanel containing buttons for selecting a file, searching the file, and quitting the program
     JPanel controlPnl;
+
+    //A JButton for selecting the file to read
     JButton fileSelectBtn;
+
+    //A JButton for searching the file based on the user's search string
     JButton searchFileBtn;
+
+    //A JButton for quitting the program
     JButton quitBtn;
+
+    //A JFileChooser for selecting a file
     JFileChooser chooser;
+
+    //A File containing the file selected by the user
     File selectedFile;
+
+    //A List<String> for storing the streamed data from the file
     List<String> originalStreamResult;
+
+    //A boolean representing whether the user has already loaded a file
     boolean hasSelectedFile = false;
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args)
     {
+        //This DataStreams object creates an instance of the DataStreams class
         DataStreams data = new DataStreams();
         data.generateFrame();
     }
 
+    /**
+     * This constructor initializes the JFileChooser
+     */
     public DataStreams()
     {
         chooser = new JFileChooser();
     }
 
+    /**
+     * This method prompts the user to select a file, displays its name, reads the file data using a Stream, and displays the file text in the originalFileTA
+     */
     public void selectFile()
     {
         File workingDirectory = new File(System.getProperty("user.dir"));
         chooser.setCurrentDirectory(workingDirectory);
 
+        //This algorithm prompts the user to select a file
         if(chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
         {
             selectedFile = chooser.getSelectedFile();
@@ -73,6 +136,7 @@ public class DataStreams
             originalFileTA.setText("");
             filteredFileTA.setText("");
 
+            //This algorithm reads the lines of the file using a Stream, appends them to the originalFileTA, and checks for exceptions
             try (Stream<String> lines = Files.lines(Paths.get(pathString)))
             {
                 originalStreamResult = lines
@@ -94,6 +158,9 @@ public class DataStreams
         }
     }
 
+    /**
+     * This method filters the selected file based on the user's normalized search string, then appends the result to the filteredFileTA
+     */
     private void filterFile()
     {
         String searchString = searchStringTF.getText().toLowerCase();
@@ -110,6 +177,12 @@ public class DataStreams
             }
     }
 
+    /**
+     * This method accepts an existing List<String>, streams it, and normalizes and filters it based on an input search String
+     * @param lines the List of Strings to be streamed and filtered
+     * @param searchString the String by which to filter the data
+     * @return a List of Strings filtered by the chosen search string
+     */
     public List<String> filterLines(List<String> lines, String searchString)
     {
         return lines.stream()
@@ -118,30 +191,42 @@ public class DataStreams
                 .collect(Collectors.toList());
     }
 
+    /**
+     * This method initializes all the panels in the GUI and adds them to a JFrame to be displayed; also sets the size, visibility, etc. of the JFrame
+     */
     private void generateFrame()
     {
+        //A JFrame for displaying the GUI
         JFrame frame = new JFrame();
         mainPnl = new JPanel();
         mainPnl.setLayout(new GridBagLayout());
         frame.add(mainPnl);
+
+        //GridBagConstraints for the titlePnl
         GridBagConstraints gbc1 = new GridBagConstraints();
         gbc1.gridx = 0;
         gbc1.gridy = 0;
         gbc1.gridwidth = 1;
         gbc1.gridheight = 1;
         gbc1.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the fileSearchPnl
         GridBagConstraints gbc2 = new GridBagConstraints();
         gbc2.gridx = 0;
         gbc2.gridy = 1;
         gbc2.gridwidth = 1;
         gbc2.gridheight = 1;
         gbc2.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the controlPnl
         GridBagConstraints gbc3 = new GridBagConstraints();
         gbc3.gridx = 0;
         gbc3.gridy = 2;
         gbc3.gridwidth = 1;
         gbc3.gridheight = 1;
         gbc3.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the FileDisplayPnl
         GridBagConstraints gbc4 = new GridBagConstraints();
         gbc4.gridx = 0;
         gbc4.gridy = 3;
@@ -179,6 +264,9 @@ public class DataStreams
         JOptionPane.showMessageDialog(null, "Welcome to the Data Stream Filter! First, select a file using the Load File button.");
     }
 
+    /**
+     * This method clears the selected file, all relevant textboxes, and program state information
+     */
     public void clearProgram()
     {
         selectedFile = null;
@@ -193,6 +281,9 @@ public class DataStreams
         searchFileBtn.setEnabled(false);
     }
 
+    /**
+     * This method establishes the titlePnl, titleLbl, and titleFont
+     */
     private void createTitlePnl()
     {
         titlePnl = new JPanel();
@@ -203,12 +294,16 @@ public class DataStreams
         titlePnl.add(titleLbl);
     }
 
+    /**
+     * This method establishes the fileSearchPnl, its layout, and its text fields and JLabels
+     */
     private void createFileSearchPnl()
     {
         fileSearchPnl = new JPanel();
         fileSearchPnl.setLayout(new GridBagLayout());
         fileSearchPnl.setBorder(new CompoundBorder(new EtchedBorder(), new EmptyBorder(10, 10, 10, 10)));
 
+        //GridBagConstraints for the fileNameLbl
         GridBagConstraints gbc1 = new GridBagConstraints();
         gbc1.gridx = 0;
         gbc1.gridy = 0;
@@ -217,12 +312,16 @@ public class DataStreams
         gbc1.fill = GridBagConstraints.NONE;
         gbc1.anchor = GridBagConstraints.EAST;
         gbc1.insets = new Insets(15, 15, 15, 15);
+
+        //GridBagConstraints for the fileNameTF
         GridBagConstraints gbc2 = new GridBagConstraints();
         gbc2.gridx = 1;
         gbc2.gridy = 0;
         gbc2.gridwidth = 1;
         gbc2.gridheight = 1;
         gbc2.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the searchStringLbl
         GridBagConstraints gbc3 = new GridBagConstraints();
         gbc3.gridx = 0;
         gbc3.gridy = 1;
@@ -231,6 +330,8 @@ public class DataStreams
         gbc3.fill = GridBagConstraints.NONE;
         gbc3.anchor = GridBagConstraints.EAST;
         gbc3.insets = new Insets(15, 15, 15, 15);
+
+        //GridBagConstraints for the searchStringTF
         GridBagConstraints gbc4 = new GridBagConstraints();
         gbc4.gridx = 1;
         gbc4.gridy = 1;
@@ -250,12 +351,16 @@ public class DataStreams
         fileSearchPnl.add(searchStringTF, gbc4);
     }
 
+    /**
+     * This method establishes the fileDisplayPnl, its layout, and its JLabels and JTextAreas
+     */
     private void createFileDisplayPnl()
     {
         fileDisplayPnl = new JPanel();
         fileDisplayPnl.setLayout(new GridBagLayout());
         fileDisplayPnl.setBorder(new CompoundBorder(new EtchedBorder(), new EmptyBorder(10, 10, 10, 10)));
 
+        //GridBagConstraints for the originalFileLbl
         GridBagConstraints gbc1 = new GridBagConstraints();
         gbc1.gridx = 0;
         gbc1.gridy = 0;
@@ -264,12 +369,16 @@ public class DataStreams
         gbc1.fill = GridBagConstraints.NONE;
         gbc1.anchor = GridBagConstraints.SOUTH;
         gbc1.insets = new Insets(15, 15, 15, 15);
+
+        //GridBagConstraints for scroller1
         GridBagConstraints gbc2 = new GridBagConstraints();
         gbc2.gridx = 0;
         gbc2.gridy = 1;
         gbc2.gridwidth = 1;
         gbc2.gridheight = 3;
         gbc2.fill = GridBagConstraints.BOTH;
+
+        //GridBagConstraints for the filteredFileLbl
         GridBagConstraints gbc3 = new GridBagConstraints();
         gbc3.gridx = 1;
         gbc3.gridy = 0;
@@ -278,6 +387,8 @@ public class DataStreams
         gbc3.fill = GridBagConstraints.NONE;
         gbc3.anchor = GridBagConstraints.SOUTH;
         gbc3.insets = new Insets(15, 15, 15, 15);
+
+        //GridBagConstraints for scroller2
         GridBagConstraints gbc4 = new GridBagConstraints();
         gbc4.gridx = 1;
         gbc4.gridy = 1;
@@ -300,6 +411,9 @@ public class DataStreams
         fileDisplayPnl.add(scroller2, gbc4);
     }
 
+    /**
+     * This method establishes the controlPnl, its layout, and the JButtons inside it
+     */
     private void createControlPnl()
     {
         controlPnl = new JPanel();
@@ -314,11 +428,12 @@ public class DataStreams
 
         fileSelectBtn.addActionListener((ActionEvent ae) ->
         {
+            //This algorithm checks if the user has loaded a file before
             if(hasSelectedFile) {
-                //This int tracks whether the user confirmed or denied they wanted to quit the program
+                //This int tracks whether the user confirmed or denied they want to load a new file
                 int selection = JOptionPane.showConfirmDialog(null, "Are you sure you want to load a new file?", "Load a File", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
 
-                //This algorithm determines whether to quit the program based on the user's input
+                //This algorithm determines whether to load a new file based on the user's input
                 if(selection == JOptionPane.YES_OPTION) {
                     clearProgram();
                     selectFile();
@@ -336,6 +451,7 @@ public class DataStreams
 
         searchFileBtn.addActionListener((ActionEvent ae) ->
         {
+            //This algorithm checks that the searchStringTF is not empty before filtering the file
             if(!searchStringTF.getText().trim().isEmpty())
             {
                 filterFile();
